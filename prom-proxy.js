ea754df1995b30144401dbe22565b9beba5ad351
TAKABOOM44 <takaboom44@users.noreply.github.com>
2026-08-11 23:53:01 +0000

    Add file logging to prom-proxy.js (prom-proxy.log) so logs are visible even when running in hidden background mode

--- prom-proxy.js
+++ prom-proxy.js
@@ -8,9 +8,18 @@
 
 const http = require('http');
 const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 8787;
 const PROM_HOST = 'my.prom.ua';
+const LOG_FILE = path.join(__dirname, 'prom-proxy.log');
+
+function log(line){
+  const stamped = `[${new Date().toISOString()}] ${line}`;
+  console.log(stamped);
+  try{ fs.appendFileSync(LOG_FILE, stamped + '\n'); }catch(e){}
+}
 
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -39,7 +48,7 @@ const server = http.createServer((req, res) => {
     if (req.headers['authorization']) headers['Authorization'] = req.headers['authorization'];
     if (body.length) headers['Content-Length'] = body.length;
 
-    console.log(`[prom-proxy] ${req.method} ${targetPath}`);
+    log(`${req.method} ${targetPath}`);
 
     const proxyReq = https.request(
       { host: PROM_HOST, path: targetPath, method: req.method, headers },
@@ -48,7 +57,7 @@ const server = http.createServer((req, res) => {
         proxyRes.on('data', (chunk) => resChunks.push(chunk));
         proxyRes.on('end', () => {
           const resBody = Buffer.concat(resChunks);
-          console.log(`[prom-proxy] відповідь ${proxyRes.statusCode}: ${resBody.toString().slice(0, 300)}`);
+          log(`відповідь ${proxyRes.statusCode}: ${resBody.toString().slice(0, 500)}`);
           res.writeHead(proxyRes.statusCode, { 'Content-Type': proxyRes.headers['content-type'] || 'application/json' });
           res.end(resBody);
         });
@@ -66,6 +75,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Prom.ua-проксі запущено: http://localhost:${PORT}`);
-  console.log('Залиш це вікно відкритим, поки працюєш із сайтом Диспетчер замовлень.');
+  log(`Prom.ua-проксі запущено: http://localhost:${PORT}`);
+  log('Залиш це вікно відкритим, поки працюєш із сайтом Диспетчер замовлень.');
 });
